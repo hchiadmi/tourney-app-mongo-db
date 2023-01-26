6c2b18caaa3f2567869ef5686b00e8b479db7047
HC <h.chiadmi@deal.fr>
2023-01-26 18:00:24 +0100

    Initial commit

--- Tourney/src/main/kotlin/com/betclic/tourney/boundary/ping/PingResource.kt
+++ Tourney/src/main/kotlin/com/betclic/tourney/boundary/ping/PingResource.kt
@@ -1,4 +1,4 @@
-package com.betclic.tourney.ping
+package com.betclic.tourney.boundary.ping
 
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RestController
